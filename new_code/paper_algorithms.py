d2f4a02abaf6bd20a621c7e3fd6a11a5d0c4fbf2
sereromain <sereromain@eisti.eu>
2021-11-21 15:09:28 +0100

    algorithms

---
+++ new_code/paper_algorithms.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def algorithm_1(m_org_d,m_dst_d, s_org_d,s_dst_d, t_start, org_d,dst_d):
+	J_d = np.array([org_d, m_org_d, m_dst_d, dst_d])
+	r = np.random.randint(2)
+
+	if r:
+		# Try to add a detour close to the origin
+		
+	else:
+
+
+	return J_d
