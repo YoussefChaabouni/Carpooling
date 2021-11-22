8a8ef00786ffed7ddb2b0d60235f54f04e24e484
Youssef Chaabouni <youssefchaabouni98@gmail.com>
2021-11-22 11:41:43 +0100

    update paper algorithms

--- new_code/paper_algorithm_1.py
+++ new_code/paper_algorithm_1.py
@@ -1,4 +1,8 @@
 import numpy as np
+from Carpooling.new_code.PersonClasses import Driver, Rider
+from Carpooling.new_code.graphClasses import Graph, MeetingPoint, Node
+from Carpooling.new_code.meansClasses import Foot
+import graphClasses
 
 def distance(a,b):
 	# calcul la distance euclidienne entre deux points de coordonnées 'a' et 'b'
@@ -36,3 +40,49 @@ def algorithm_1(m_org_d,m_dst_d, s_org_d,s_dst_d, org_d,dst_d, t_start):
 				J_d = [org_d, m_org_d, s_org_d, s_dst_d, m_dst_d, dst_d]
 
 	return J_d
+
+
+# define walk function
+
+
+'''
+define walk function
+inputs : node1,node2
+outputs : walking_time
+'''
+def walk(d:Node,m:Node):
+
+	distance = Graph.get_distance(d,m)
+	speed = Foot.get_speed
+	return distance/speed
+
+
+def algorithm_2(rider: Rider,d: Driver,m_board: MeetingPoint,m_out: MeetingPoint):
+
+	z = rider.get_pos_depart
+	z_prime = rider.get_pos_arrivee
+	t = rider.get_born_time
+
+	# initialise to infinity
+	t_arrivee = float('inf')
+	t_waiting = float("inf")
+	walk_distance = float("inf")
+
+	# check if there is a seat available
+	idx_board = d.get_journey.index(m_board)
+	idx_out = d.get_journey.index(m_out)
+
+	# check the capacity in all stops between m_board and m_out
+	for i in range(idx_board,idx_out):
+		if d.max_capacity - d.get_current_capacity[i] <= 0:
+			return
+	
+	t_prime = t + walk(z,m_board)
+	walk_distance = Graph.get_distance(z,m_board)
+
+	##'''if t_prime > d.get_trajectory.get_trajectory_list
+
+
+
+
+	return 0
