a3f97f1a89a5ca608fa61d6a795eba8e016499a3
sereromain <sereromain@eisti.eu>
2021-11-22 12:40:53 +0100

    algo 1 correct

--- new_code/paper_algorithm_1.py
+++ new_code/paper_algorithm_1.py
@@ -20,18 +20,18 @@ def algorithm_1(m_org_d,m_dst_d, s_org_d,s_dst_d, org_d,dst_d, t_start):
 
 	if r:
 		# Try to add a detour close to the origin
-		if  distance(m_org_d,s_org_d) + distance(s_org_d,m_dst_d) > 1.15 * distance(m_org_d,m_dst_d):
+		if  distance(m_org_d,s_org_d) + distance(s_org_d,m_dst_d) <= 1.15 * distance(m_org_d,m_dst_d):
 			# Add a detour through station s_org_d
 			J_d = [org_d, m_org_d, s_org_d, m_dst_d, dst_d]
-			if distance(m_org_d,s_org_d) + distance(s_org_d,s_dst_d) + distance(s_org_d,m_dst_d) > 1.15 * distance(m_org_d,m_dst_d):
+			if distance(m_org_d,s_org_d) + distance(s_org_d,s_dst_d) + distance(s_org_d,m_dst_d) <= 1.15 * distance(m_org_d,m_dst_d):
 				# Also add a detour through the station close to the driver destination
 				J_d = [org_d, m_org_d, s_org_d, s_dst_d, m_dst_d, dst_d]
 	else:
 		# Try to add a detour close to the destination
-		if  distance(m_org_d,s_dst_d) + distance(s_dst_d,m_dst_d) > 1.15 * distance(m_org_d,m_dst_d):
+		if  distance(m_org_d,s_dst_d) + distance(s_dst_d,m_dst_d) <= 1.15 * distance(m_org_d,m_dst_d):
 			# Add a detour through station s_dst_d
 			J_d = [org_d, m_org_d, s_dst_d, m_dst_d, dst_d]
-			if distance(m_org_d,s_org_d) + distance(s_org_d,s_dst_d) + distance(s_org_d,m_dst_d) > 1.15 * distance(m_org_d,m_dst_d):
+			if distance(m_org_d,s_org_d) + distance(s_org_d,s_dst_d) + distance(s_org_d,m_dst_d) <= 1.15 * distance(m_org_d,m_dst_d):
 				# Also add a detour through the station close to the driver origin
 				J_d = [org_d, m_org_d, s_org_d, s_dst_d, m_dst_d, dst_d]
 
