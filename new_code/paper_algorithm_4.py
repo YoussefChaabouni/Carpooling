c9c7968b68c53309415436b2f05b9c6c242f2570
sereromain <sereromain@eisti.eu>
2021-11-22 11:12:09 +0100

    debut algo 4

---
+++ new_code/paper_algorithm_4.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def algorithm_4():
+
+	# --------------- FIRST MILE --------------- #
+	for d in drivers:
+		if m_org_d == m_org_r and s_org_r in d_trajectory :
+			# APPLIQUER L'ALGORITHME 2
+
+			
+
+
+	# --------------- LAST MILE --------------- #
+
+	return 0
