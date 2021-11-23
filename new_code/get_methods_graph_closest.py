e05ef0fda62ce45d1b3c76b5d244b0bf7d2d9d7c
sereromain <sereromain@eisti.eu>
2021-11-23 14:32:08 +0100

    méthodes de plus proches nodes

---
+++ new_code/get_methods_graph_closest.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# POUR LA CLASSE NODE
+# ------------------------------- #
+def get_xy_coordinate(self):
+    return np.array([self.x_coordinate,self.y_coordinate])
+# ------------------------------- #
+
+
+# POUR LA CLASSE GRAPH
+# ------------------------------- #
+def get_closest_MP_or_Station(self,nodes): # on prend en entrée une liste de nodes
+    V = np.zeros((len(nodes),2))
+
+    for i,n in enumerate(nodes):
+        V[i] = nodes.get_xy_coordinate()
+
+    org_x,dst_x = np.meshgrid(V[:,0],V[:,0])
+    org_y,dst_y = np.meshgrid(V[:,1],V[:,1])
+
+    # pour éviter que la node se choisisse elle-même comme node la plus proche on rajoute un poid de +infini sur la diagonale de la matrice des distances²
+    I = np.diag([np.inf]*len(nodes))
+
+    argmin_distance = np.argmin((org_x-dst_x)**2 + (org_y-dst_y)**2 + I,axis=0)
+
+    return argmin_distance
+# ------------------------------- #
