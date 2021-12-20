47857f31ded2db38d93a947d6922ca1f32f77141
Youssef Chaabouni <youssefchaabouni98@gmail.com>
2021-12-20 11:42:09 +0100

    added no carpooling system

---
+++ new_code/no_carpooling_system.py
@@ -0,0 +1,123 @@
+from typing import List
+import numpy as np
+from numpy.core.numeric import Infinity
+from PersonClasses import Rider, Driver
+from graphClasses import Graph
+from helperFunctions import walk
+from check_algorithm_3 import check_algorithm_3
+from meansClasses import Foot
+from helperFunctions import board_train, next_train_time
+from helperFunctions import Drive
+from paper_algorithm_3 import algorithm_3
+from paper_algorithm_2 import algorithm_2
+from transit_only import transit_only_algorithm
+
+
+'''
+This function will simulate the decision making process of the rider in a no carpooling system
+he will choose between 2 things : 
+
+- Go on foot
+- use transit 
+
+this function will return:
+t_prime (float): the time taken by the rider in the optimal ride (it's infinity if the rider isn't able to reach r_dst)
+solution (string): string expressing the solution the rider has adopted 
+(it's "no solution" if the rider can't reach his destination) 
+
+remarks :
+- this algorithm will update the riders trajectory according to the adopted solution
+'''
+def no_carpooling_system(rider : Rider,drivers : List[Driver],graph: Graph):
+
+    t_prime = np.Infinity
+    solution = "no solution"
+    
+    solution_encoding = ["foot","transit"]
+    print("NO CARPOOLING SYSTEM RESULTS :")
+    print("_______ESTIMATIONS_________________")
+    t_foot_prime = walk(graph.get_node(rider.pos_depart),graph.get_node(rider.pos_arrivee),graph,5/60)
+    t_transit = transit_only_algorithm(rider,graph)
+
+    
+    # if the rider walks more than 2.5km we set t_foot to infinity
+    if graph.get_distance(graph.get_node(rider.pos_depart),graph.get_node(rider.pos_arrivee)) > 2.5 :
+        t_foot = np.Infinity
+    else:
+        t_foot = t_foot_prime
+
+    
+    print("FOOT : ")
+    print("    estimated foot time = ",t_foot_prime)
+    print("    estimated walking distance = ",graph.get_distance(graph.get_node(rider.pos_depart),graph.get_node(rider.pos_arrivee)))
+    
+    print("________RIDER DECISION PROCESS___________")
+    
+    #print("     estimated time to carpool = ",t_carpool)
+    print(" time to go on FOOT = ",t_foot)
+    print(" time to TRANSIT = ",t_transit)
+
+
+    solution_times_list = [t_foot,t_transit]
+
+
+    # see if all the results are infinite and return the solution with best time
+    if t_foot == np.Infinity  and t_transit == np.Infinity:
+        print("the rider has no solution available to him")
+
+        # we update the waiting time to infinity
+        rider.update_waiting_time(np.Infinity)
+        solution = "no solution"
+
+    else :
+        
+        idx_min = np.argmin(np.array([t_foot,t_transit])) # minimum solution index
+        solution = solution_encoding[idx_min] # adopted solution label
+        t_prime = solution_times_list[idx_min] # adopted solution time
+
+        #_____________________________RIDER INFORMATION UPDATES________________________________
+
+        # get origin and destination nodes
+        r_org = graph.get_node(rider.pos_depart)
+        r_dst = graph.get_node(rider.pos_arrivee)
+
+
+        if solution == "foot":
+            print("the rider chooses to go on foot")
+            arrival_time_destination = rider.born_time + t_foot
+            walking_distance = graph.get_distance(graph.get_node(rider.pos_depart),graph.get_node(rider.pos_arrivee))
+
+            # update rider information
+            rider.update_walking_distance(walking_distance)
+            rider.get_trajectory().update_trajectory(Foot(ID="walk only",Speed=5/60),arrival_time_destination,arrival_time_destination,r_dst.get_id())
+
+            return t_prime , solution
+        if solution == "transit":
+            print("the rider chooses to go with transit")
+            # get stations
+            s_org = graph.get_closest_MP_or_Station(r_org,"Stations")
+            s_dst = graph.get_closest_MP_or_Station(r_dst,"Stations")
+
+            # computing different arrival, departure and waiting times 
+            arrival_time_origin_station = rider.born_time + walk(r_org,s_org,graph,5/60) + 1
+            waiting_for_train = next_train_time(s_org,s_dst,arrival_time_origin_station)
+            departure_time_origin_station = arrival_time_origin_station + waiting_for_train
+            arrival_time_destination_station = board_train(s_org,s_dst,arrival_time_origin_station)
+            departure_time_destination_station = arrival_time_destination_station + 1
+            arrival_time_destination = departure_time_destination_station + walk(s_dst,r_dst,graph,5/60)
+
+            #overall walking distance in this option
+            walking_distance = graph.get_distance(r_org,s_org) + graph.get_distance(s_dst,r_dst)
+
+            # update rider information
+            rider.update_waiting_time(waiting_for_train - arrival_time_origin_station)
+            rider.update_walking_distance(walking_distance)
+            # walk to station
+            rider.get_trajectory().update_trajectory(Foot(ID="walk to first station",Speed=5/60),arrival_time_origin_station,departure_time_origin_station,s_org.get_id())
+            # train ride
+            rider.get_trajectory().update_trajectory(Foot(ID="train from "+s_org.get_id()+" to "+s_dst.get_id(),Speed=80/60),arrival_time_destination_station,departure_time_destination_station,s_dst.get_id())
+            # walk to destination
+            rider.get_trajectory().update_trajectory(Foot(ID="walk to destination "+r_dst.get_id(),Speed=5/60),arrival_time_destination,arrival_time_destination,r_dst.get_id())
+     
+
+    return t_prime , solution
---
+++ new_code/no_carpooling_test.py
@@ -0,0 +1,166 @@
+import numpy as np
+from meansClasses import Foot
+from helperFunctions import get_timetable
+from current_system import current_system
+from statistics import vehicle_maximum_occupancy
+from statistics import average_walking_and_waiting_time
+from no_carpooling_system import no_carpooling_system
+from paper_algorithm_4 import algorithm_4
+from paper_algorithm_3 import algorithm_3
+
+from paper_algorithm_1 import algorithm_1
+from paper_algorithm_2 import algorithm_2
+from graphClasses import MeetingPoint, Station, Graph, Trajectory
+from PersonClasses import Driver, Rider
+from helperFunctions import Drive
+import time
+
+start = time.time()
+NUMBER_OF_MPS = 50
+NUMBER_OF_STATIONS = 10
+TAILLE_DE_MAP = 10 # en km
+
+# CREATION DES NODES
+NODES = []
+x,y = np.random.random((2,NUMBER_OF_MPS)) * TAILLE_DE_MAP
+#print(x,y)
+for i in range(NUMBER_OF_MPS):
+	NODES.append(MeetingPoint(ID="MP"+str(i),x_coord=x[i], y_coord=y[i]))
+
+x,y = np.random.random((2,NUMBER_OF_STATIONS)) * TAILLE_DE_MAP
+#print(x,y)
+
+#liste_droite = [0,1,11]
+#liste_gauche = [11 - x for x in liste_droite]
+
+# if s_org < s_dest : 
+# 	s_org.get_liste_droite()
+# else:
+# 	graph.get_node(s_org).get_liste_gauche()   
+duration_of_simulation = 180
+train_frequency = 5
+number_of_trains_per_sim = int(duration_of_simulation/train_frequency)
+
+#list_ids_stations = ["S0","S1","S2","S3"] -> [0,1,1.5,0.5] -> [0,2,3,1] -> [0,2,5,6]
+
+
+list_id_stations = []
+
+for i in range(NUMBER_OF_STATIONS):
+	list_id_stations.append("S"+str(i))
+	NODES.append(Station(ID="S"+str(i),x_coord=x[i], y_coord=y[i]))
+
+# INITIALISATION DU GRAPH
+G = Graph(node_list = NODES)
+
+timetable_gauche , timetable_droite = get_timetable(G,80,list_id_stations,number_of_trains_per_sim)
+
+# add timetables to stations
+for i in range(len(list_id_stations)):
+	G.get_node(list_id_stations[i]).liste_gauche = timetable_gauche[:,i].tolist()
+	G.get_node(list_id_stations[i]).liste_droite = timetable_droite[:,i].tolist()
+
+#print("liste gauche de S0 = ",G.get_node(list_id_stations[0]).liste_gauche)
+
+print("graph of this many nodes = ",len(G.node_list))
+'''
+# INITIALISATION DU DRIVER
+d = Driver(pos_depart="MP0",
+	pos_arrivee="MP5",
+	ID_user = "D0",
+	born_time = 10,
+	ID_car="C0",
+	Speed=40/60,
+	max_capacity=4,
+	current_capacity=[],
+	riders_list=[],
+	trajectory=Trajectory())
+
+d.trajectory = Trajectory(means_list=[d],arr_time_list=[d.born_time],dep_time_list=[d.born_time],node_list=[d.pos_depart])
+
+d1 = Driver(pos_depart="MP1",
+	pos_arrivee="MP5",
+	ID_user = "D1",
+	born_time = 20,
+	ID_car="C1",
+	Speed=40/60,
+	max_capacity=4,
+	current_capacity=[],
+	riders_list=[], 
+	trajectory=Trajectory())
+
+d1.trajectory = Trajectory(means_list=[d1],arr_time_list=[d1.born_time],dep_time_list=[d1.born_time],node_list=[d1.pos_depart])
+
+'''
+# try out many drivers
+drivers = []
+for i in range(500):
+
+    # generate random origin, destination and born time
+    random_born_time = np.random.randint(0,60)
+    n_org = np.random.randint(0,NUMBER_OF_MPS-1)
+    n_dest = n_org
+    while n_dest == n_org :
+        n_dest = np.random.randint(0,NUMBER_OF_MPS-1)
+
+    # initialise drivers
+    d = Driver(pos_depart="MP"+str(n_org),
+	pos_arrivee="MP"+str(n_dest),
+	ID_user = "D"+str(i),
+	born_time = random_born_time,
+	ID_car="C"+str(i),
+	Speed=40/60,
+	max_capacity=4,
+	current_capacity=[],
+	riders_list=[],
+	trajectory=Trajectory())
+
+    d.trajectory = Trajectory(means_list=[d],arr_time_list=[d.born_time],dep_time_list=[d.born_time],node_list=[d.pos_depart])
+
+    drivers.append(d)
+
+
+riders_list = []
+NUMBER_OF_RIDERS = 100
+for j in range(NUMBER_OF_RIDERS):
+
+	random_born_time = np.random.randint(0,20)
+	n_org = np.random.randint(0,NUMBER_OF_MPS-1)
+	n_dest = n_org
+	while n_dest == n_org :
+		n_dest = np.random.randint(0,NUMBER_OF_MPS-1)
+
+	r = Rider(pos_depart = "MP"+str(n_org),pos_arrivee = "MP"+str(n_dest),ID = "R"+str(j),born_time=random_born_time,trajectory=Trajectory())
+	r.trajectory = Trajectory(means_list=[Foot(Speed=5/60,ID="init "+r.get_id())],arr_time_list=[r.born_time],dep_time_list=[r.born_time],node_list=[r.pos_depart])
+	riders_list.append(r)
+
+
+
+#print(new_list)
+print("___________________ALGORITHM 1_________________________________")
+
+for d in drivers :
+    journey = algorithm_1(d,G)
+    d.set_journey(journey)
+    print("trajectory of ",d.get_id()," = ",d.get_trajectory().node_id_list)
+
+carpooling = 0
+foot = 0
+transit = 0
+no_solution = 0
+
+
+for r in riders_list:
+	print("______________________CURRENT SYSTEM_________________________________")
+	print("___________________FOR RIDER : ",r.get_id(),"_________________________")
+	solution = no_carpooling_system(r,drivers,G)[1]
+
+	print("____________RIDER INFORMATION POST CURRENT SYSTEM______________________")
+	print('rider trajectory = ',r.get_trajectory().node_id_list)
+	print("arrival times of rider = ",r.get_trajectory().arr_time_list)
+	print("waiting time of rider = ",r.waiting_time)
+	print("walking distance of rider = ",r.walking_distance)
+
+average_walking , average_waiting = average_walking_and_waiting_time(riders_list,system="Current")
+print(average_walking)
+print(average_waiting)
